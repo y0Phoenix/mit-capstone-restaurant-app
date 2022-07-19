9354fa1dafb726d728f05b29c4c82d08b1c9734f
Aaron <aarongraybill3@gmail.com>
2022-07-18 19:47:20 -0500

    init cart route

---
+++ backend/typescript/routes/cart.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+const router = express.Router();
+import { check, validationResult } from 'express-validator';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import gravatar from 'gravatar';
+import Cart from '../classes/Cart';
+import auth from '../middleware/auth';
+
+import User from '../schemas/User';
+
+/**
+ * @POST
+ * @desc update user cart
+ */
+router.post('/', auth, async (req: any, res) => {
+    interface Body {
+        cart: Cart
+    };
+    try {
+        const {cart}: Body = req.body;
+        
+        // update user cart
+        req.user.cart = cart;
+
+        // save data
+        await req.user.save()
+
+        res.json({data: req.user, error: false});
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({msgs: [{msg: 'Server Error O1'}], error: true});
+    }
+});
+
+export default router;
