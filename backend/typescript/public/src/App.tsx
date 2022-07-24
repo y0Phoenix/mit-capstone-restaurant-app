8dd44c5595b8c6ab50f4ed0f22ae7f89ab0d6453
Aaron <aarongraybill3@gmail.com>
2022-07-24 18:59:57 -0500

    finish admin landing page

--- backend/typescript/public/src/App.tsx
+++ backend/typescript/public/src/App.tsx
@@ -2,7 +2,6 @@ import React, {Dispatch, Fragment, useEffect, useState} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import './App.css'
 import Landing from './routes/Landing';
 
 function App() {
@@ -11,9 +10,11 @@ function App() {
     <Provider store={store}>
       <Router>
         <Fragment>
-          <Routes>
-            <Route path='/' element={<Landing/>}/>
-          </Routes>
+          <div className='main'>
+            <Routes>
+              <Route path='/' element={<Landing/>}/>
+            </Routes>
+          </div>
         </Fragment>
       </Router>
     </Provider>
--- backend/typescript/public/src/routes/Landing.tsx
+++ backend/typescript/public/src/routes/Landing.tsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Card, Form, InputGroup, Button } from 'react-bootstrap';
+import { motion } from 'framer-motion';
+import pic from '../../pictures/logo_50.png';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    remember: false
+  });
+  const {
+    email,
+    password,
+    remember
+  } = formData;
+
+  const onsubmit = (e: any) => {};
+  const onchange = (e: any) => {};
   return (
-    <div>Poop</div>
+    <div className='landing'>
+      <Card style={{width: '425px', height: '750px'}}>
+        <Card.Img variant='top' src={pic}></Card.Img>
+        <Card.Body>
+          <Card.Title>Login To Your Admin Dashboard</Card.Title>
+          <br></br>
+          <form autoComplete='off' onSubmit={e => onsubmit(e)}>
+            <Card.Text>
+              <InputGroup className='mb-3'>
+                <InputGroup.Text id='basic-addon1'>*</InputGroup.Text>
+                <Form.Control
+                  placeholder='Email'
+                  value={email}
+                  name='email'
+                  onChange={e => onchange(e)}>
+                </Form.Control>
+                <InputGroup.Text id='basic-addon2'>@example.com</InputGroup.Text>
+              </InputGroup>
+              <InputGroup className='mb-3'>
+                <InputGroup.Text id='basic-addon1'>*</InputGroup.Text>
+                <Form.Control
+                  placeholder='Password'
+                  value={password}
+                  name='password'
+                  onChange={e => onchange(e)}>
+                </Form.Control>
+                <InputGroup.Text id='basic-addon2'>atleat 6 characters</InputGroup.Text>
+              </InputGroup>
+              <div className='remember-me'>
+                <input type={'checkbox'} checked={remember} name='remeber' onChange={e => onchange(e)}></input>
+                <small>
+                  Remeber Me
+                </small>
+              </div>
+              <small>
+                <Link to={'/register'} className='link'>
+                  Or Create Account
+                </Link>
+              </small>
+              <br></br>
+              <motion.button className='btn btn-dark' whileHover={{scale: 1.07}} type='submit'>
+                Login
+              </motion.button>
+            </Card.Text>
+          </form>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
