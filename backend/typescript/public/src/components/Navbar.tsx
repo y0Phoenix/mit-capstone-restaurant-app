56cf929b04854bafb584787a8ba3779fd14036d3
Aaron <aarongraybill3@gmail.com>
2022-07-27 19:48:09 -0500

    added navbar

--- backend/typescript/public/src/components/Navbar.tsx
+++ backend/typescript/public/src/components/Navbar.tsx
@@ -1,27 +1,36 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <Nav className='flex-column'>
+    <Nav variant='pills' defaultActiveKey={'home'} className='flex-column'>
         <Nav.Item>
-            <Nav.Link href='/home'>
-                Home <i className='fa-solid fa-chart-line'></i>
+            <Nav.Link eventKey={'home'}>
+                <Link to={'/home'} className='link'>
+                    Home <i className='fa-solid fa-chart-line'></i>
+                </Link>
             </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link href='/restaurants'>
-                Restaurants <i className='fa-solid fa-utensils'></i>
+            <Nav.Link eventKey={'restaurant'}>
+                <Link to={'/restaurants'} className='link'>
+                    Restaurants <i className='fa-solid fa-utensils'></i>
+                </Link>
             </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link href='/orders'>
-                Orders <i className='fa-solid fa-file-invoice'></i>
+            <Nav.Link eventKey={'orders'}>
+                <Link to={'/orders'} className='link'>
+                    Orders <i className='fa-solid fa-file-invoice'></i>
+                </Link>
             </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link href='/account'>
-                Account Management <i className='fa-solid fa-user'></i>
+            <Nav.Link eventKey={'account'}>
+                <Link to={'/account'} className='link'>
+                    Account Management <i className='fa-solid fa-user'></i>
+                </Link>
             </Nav.Link>
         </Nav.Item>
     </Nav>
