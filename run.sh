f3923764a53b069b04820eecea595a5d2590cb36
Aaron <aarongraybill3@gmail.com>
2022-08-02 21:16:17 -0500

    improved .sh file

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+npm i -g typescript
 if [$TYPE = "FRONT_END"]
 then
+	npm i --prefix /app/backend
+	npm i --prefix /app/backend/typescript/public
 	npm run build --prefix /app/backend
 	npm start --prefix /app/backend
 	echo "Running Backend Script"
 	echo $TYPE
 else
+	npm i --prefix /app/client
 	npm run build --prefix /app/client
 	echo "Running Frontend Scripts"
 	echo $TYPE
