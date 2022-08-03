de53bc4a5b0003dfba71a8fe7d5a939287d78b33
Aaron <aarongraybill3@gmail.com>
2022-08-02 21:24:27 -0500

    improvemtnets to .sh file

--- run.sh
+++ run.sh
@@ -6,14 +6,22 @@ then
 	echo "Running Backend Script"
 	echo $TYPE
 	npm i --prefix /app/backend
+	echo "npm i --prefix /app/backend"
 	npm i --prefix /app/backend/typescript/public
+	echo "npm i --prefix /app/backend/typescript/public"
 	npm i --save-dev @types/uuid /app/backend/typescript/public
+	echo "npm i --save-dev @types/uuid /app/backend/typescript/public"
 	npm run build --prefix /app/backend
+	echo "npm run build --prefix /app/backend"
 	npm start --prefix /app/backend
+	echo "npm start --prefix /app/backend"
 else
 	echo "Running Frontend Scripts"
 	echo $TYPE
 	npm i --prefix /app/client
-	/app/backend/typescript/public /app/client
+	echo "npm i --prefix /app/client"
+	npm i --save-dev @types/uuid /app/client
+	echo "npm i --save-dev @types/uuid /app/client"
 	npm run build --prefix /app/client
+	echo "npm run build --prefix /app/client"
 fi
